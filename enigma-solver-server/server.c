490aa1ea025e5d02b7e8c13123f51d9b2806ea84
Frode Roxrud Gill <frodegill@gmail.com>
2015-07-26 22:49:20 +0200

    Server should be working now

--- enigma-solver-server/server.c
+++ enigma-solver-server/server.c
@@ -9,15 +9,15 @@
 #include <cstring>
 #include <fstream>
 #include <ios>
+#include <list>
 #include <netdb.h>
 #include <string>
 #include <stdlib.h>
 #include <unistd.h>
-#include <vector>
 
 
 #define REFLECTOR_COUNT (2) //B and C
-#define RING_COUNT (5)//I, II, III, IV and V
+#define RING_COUNT (8)//I, II, III, IV, V, VI, VII and VIII
 #define PACKET_COUNT (REFLECTOR_COUNT*RING_COUNT*(RING_COUNT-1)*(RING_COUNT-2))
 
 
@@ -32,6 +32,7 @@
 # define DEFAULT_PORT  "2720" //Same as above, as string
 #endif
 
+#define NOT_FOUND (-1)
 #define EQUAL (0)
 #define BACKLOG (10)
 #define RECV_BUFFER_LENGTH (8*1024)
@@ -43,13 +44,19 @@
 std::string g_words;
 std::string g_encrypted_text;
 
+int g_max_score = 0;
+int g_max_ring_key_settings = 0;
+std::string g_max_plugboard;
+std::string g_max_plaintext;
+
+
 char* g_recv_buffer;
 
 struct PacketInfo {
 	int m_packet;
 	time_t m_start_time;
 };
-std::vector<PacketInfo*> g_packets;
+std::list<PacketInfo> g_packets;
 
 
 bool ReadWordlist(const char* wordlist_filename)
@@ -182,38 +189,218 @@ int CreateSocket(const char* port_str) // Code based on Beej's Guide to Network 
 	return socket_fd;
 }
 
-void HandleClient(int& /*packet*/, int client_fd)
+bool SendBuffer(int client_fd, const char* buffer, size_t buffer_length)
+{
+	int bytes_sent;
+	while (0<buffer_length)
+	{
+		if (-1 == (bytes_sent=send(client_fd, buffer, buffer_length, DEFAULT_SEND_FLAGS)))
+			return false;
+
+		buffer += bytes_sent;
+		buffer_length -= bytes_sent;
+	}
+	return true;
+}
+
+int FindPendingPacket()
+{
+	time_t find_time = time(NULL) - MAX_CALC_TIME_SEC;
+	for (std::list<PacketInfo>::iterator iter = g_packets.begin(); iter != g_packets.end(); ++iter)
+	{
+		PacketInfo packet_info = *iter;
+		if (find_time > packet_info.m_start_time)
+		{
+			return packet_info.m_packet;
+		}
+	}
+	return NOT_FOUND;
+}
+
+void RemovePendingPacket(int packet)
+{
+	for (std::list<PacketInfo>::iterator iter = g_packets.begin(); iter != g_packets.end(); ++iter)
+	{
+		PacketInfo packet_info = *iter;
+		if (packet == packet_info.m_packet)
+		{
+			g_packets.erase(iter);
+			break;
+		}
+	}
+}
+
+void RegisterPacket(int packet)
+{
+	time_t current_time = time(NULL);
+	for (std::list<PacketInfo>::iterator iter = g_packets.begin(); iter != g_packets.end(); ++iter)
+	{
+		PacketInfo packet_info = *iter;
+		if (packet == packet_info.m_packet)
+		{
+			packet_info.m_start_time = current_time;
+			return;
+		}
+	}
+
+	PacketInfo packet_info;
+	packet_info.m_packet = packet;
+	packet_info.m_start_time = current_time;
+	g_packets.push_back(packet_info);
+}
+
+void SendPacket(int& packet, int client_fd)
+{
+	int client_packet = FindPendingPacket();
+	if (NOT_FOUND == client_packet)
+	{
+		client_packet = packet++;
+	}
+
+	if (PACKET_COUNT <= client_packet)
+	{
+		SendBuffer(client_fd, "DONE", 4);
+	}
+	else
+	{
+		sprintf(g_recv_buffer, "PACKET %d\r\n", client_packet);
+		if (!SendBuffer(client_fd, g_recv_buffer, strlen(g_recv_buffer))) return;
+		RegisterPacket(client_packet);
+	}
+}
+
+void SkipWhitespace(int client_fd, char*& g_recv_buffer, size_t& buffer_pos)
+{
+	while (true)
+	{
+		if (0 == g_recv_buffer[buffer_pos])
+		{
+			int received_bytes = recv(client_fd, g_recv_buffer, RECV_BUFFER_LENGTH, DEFAULT_RECV_FLAGS);
+			if (-1 == received_bytes) return;
+			
+			g_recv_buffer[received_bytes] = 0;
+			buffer_pos = 0;
+		}
+
+		if (' '!=g_recv_buffer[buffer_pos])
+			return;
+
+		buffer_pos++;
+	}
+}
+
+bool ParseInt(int client_fd, char*& g_recv_buffer, size_t& buffer_pos, int& result)
+{
+	SkipWhitespace(client_fd, g_recv_buffer, buffer_pos);
+
+	bool found_result = false;
+	result = 0;
+	while (true)
+	{
+		if (0 == g_recv_buffer[buffer_pos])
+		{
+			int received_bytes = recv(client_fd, g_recv_buffer, RECV_BUFFER_LENGTH, DEFAULT_RECV_FLAGS);
+			if (-1 == received_bytes) break;
+			
+			g_recv_buffer[received_bytes] = 0;
+			buffer_pos = 0;
+		}
+
+		if ('0'>g_recv_buffer[buffer_pos] || '9'<g_recv_buffer[buffer_pos]) break;
+
+		found_result = true;
+		result = result*10 + g_recv_buffer[buffer_pos++]-'0';
+	}
+	return found_result;
+}
+
+bool ParseString(int client_fd, char*& g_recv_buffer, size_t& buffer_pos, std::string& result)
+{
+	SkipWhitespace(client_fd, g_recv_buffer, buffer_pos);
+
+	bool found_result = false;
+	result.clear();
+	while (true)
+	{
+		if (0 == g_recv_buffer[buffer_pos])
+		{
+			int received_bytes = recv(client_fd, g_recv_buffer, RECV_BUFFER_LENGTH, DEFAULT_RECV_FLAGS);
+			if (-1 == received_bytes) break;
+			
+			g_recv_buffer[received_bytes] = 0;
+			buffer_pos = 0;
+		}
+
+		if ('A'>g_recv_buffer[buffer_pos] || 'Z'<g_recv_buffer[buffer_pos]) break;
+
+		found_result = true;
+		result.append(1, g_recv_buffer[buffer_pos++]);
+	}
+	return found_result;
+}
+
+void HandleClient(int& packet, int client_fd)
 {
 	int received_bytes = recv(client_fd, g_recv_buffer, RECV_BUFFER_LENGTH, DEFAULT_RECV_FLAGS);
+	if (-1 == received_bytes) return;
+	
 	g_recv_buffer[received_bytes] = 0;
-	if (EQUAL == strncmp("NEW ", g_recv_buffer, 4))
+	
+	if (EQUAL == strncmp("STATUS", g_recv_buffer, 6))
 	{
-		int bytes_sent;
-		const char* send_buffer = g_words.c_str();
-		int send_buffer_length = strlen(send_buffer);
-		if (-1 == (bytes_sent=send(client_fd, "WORDS ", 6, DEFAULT_SEND_FLAGS))) return;
-		while (0<send_buffer_length)
-		{
-			if (-1 == (bytes_sent=send(client_fd, send_buffer, send_buffer_length, DEFAULT_SEND_FLAGS))) return;
-			send_buffer += bytes_sent;
-			send_buffer_length -= bytes_sent;
-		}
-		if (-1 == (bytes_sent=send(client_fd, "\r\n", 2, DEFAULT_SEND_FLAGS))) return;
+		sprintf(g_recv_buffer, "PROGRESS %d/%d\r\n", packet, PACKET_COUNT);
+		if (!SendBuffer(client_fd, g_recv_buffer, strlen(g_recv_buffer))) return;
 
-		send_buffer = g_encrypted_text.c_str();
-		send_buffer_length = strlen(send_buffer);
-		if (-1 == (bytes_sent=send(client_fd, "TEXT ", 5, DEFAULT_SEND_FLAGS))) return;
-		while (0<send_buffer_length)
-		{
-			if (-1 == (bytes_sent=send(client_fd, send_buffer, send_buffer_length, DEFAULT_SEND_FLAGS))) return;
-			send_buffer += bytes_sent;
-			send_buffer_length -= bytes_sent;
-		}
-		if (-1 == (bytes_sent=send(client_fd, "\r\n", 2, DEFAULT_SEND_FLAGS))) return;
+		int client_count = g_packets.size();
+		sprintf(g_recv_buffer, "CLIENTS %d\r\n", client_count);
+		if (!SendBuffer(client_fd, g_recv_buffer, strlen(g_recv_buffer))) return;
 
+		sprintf(g_recv_buffer, "MAX_SCORE %d\r\n", g_max_score);
+		if (!SendBuffer(client_fd, g_recv_buffer, strlen(g_recv_buffer))) return;
+
+		sprintf(g_recv_buffer, "MAX_RING_KEY %d\r\n", g_max_ring_key_settings);
+		if (!SendBuffer(client_fd, g_recv_buffer, strlen(g_recv_buffer))) return;
+
+		if (!SendBuffer(client_fd, "MAX_PLUGBOARD ", 14)) return;
+		if (!SendBuffer(client_fd, g_max_plugboard.c_str(), g_max_plugboard.length())) return;
+		if (!SendBuffer(client_fd, "\r\n", 2)) return;
+
+		if (!SendBuffer(client_fd, "MAX_PLAINTEXT ", 14)) return;
+		if (!SendBuffer(client_fd, g_max_plaintext.c_str(), g_max_plaintext.length())) return;
+		if (!SendBuffer(client_fd, "\r\n", 2)) return;
 	}
-	else if (0 == strncmp("DONE ", g_recv_buffer, 5))
+	else if (EQUAL == strncmp("NEW", g_recv_buffer, 3))
 	{
+		if (!SendBuffer(client_fd, "WORDS ", 6)) return;
+		if (!SendBuffer(client_fd, g_words.c_str(), g_words.length())) return;
+		if (!SendBuffer(client_fd, "\r\n", 2)) return;
+
+		if (!SendBuffer(client_fd, "TEXT ", 5)) return;
+		if (!SendBuffer(client_fd, g_encrypted_text.c_str(), g_encrypted_text.length())) return;
+		if (!SendBuffer(client_fd, "\r\n", 2)) return;
+
+		SendPacket(packet, client_fd);
+	}
+	else if (EQUAL == strncmp("DONE ", g_recv_buffer, 5)) //"DONE <packet> <score> <ring/key-settings> <plugboard> <plaintext>"
+	{
+		size_t buffer_pos = 5;
+		int client_packet;
+		if (!ParseInt(client_fd, g_recv_buffer, buffer_pos, client_packet)) return;
+		RemovePendingPacket(client_packet);
+		
+		int client_score;
+		if (!ParseInt(client_fd, g_recv_buffer, buffer_pos, client_score)) return;
+		if (g_max_score < client_score)
+		{
+			g_max_score = client_score;
+			ParseInt(client_fd, g_recv_buffer, buffer_pos, g_max_ring_key_settings);
+			ParseString(client_fd, g_recv_buffer, buffer_pos, g_max_plugboard);
+			ParseString(client_fd, g_recv_buffer, buffer_pos, g_max_plaintext);
+
+			fprintf(stdout, "New high: %d %d %s %s\n", g_max_score, g_max_ring_key_settings, g_max_plugboard.c_str(), g_max_plaintext.c_str());
+		}
+
+		SendPacket(packet, client_fd);
 	}
 
 }
@@ -231,8 +418,6 @@ void MainLoop(int socket_fd)
 		int client_fd = accept(socket_fd, (struct sockaddr*)&their_addr, &addr_size);
 		HandleClient(packet, client_fd);
 		close(client_fd);
-
-		packet++;
 	}
 }
 
